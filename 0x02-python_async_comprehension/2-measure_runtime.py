6a78c808ee8816415797f09354f42cc113a6c2c6
Davies Brown <nwosudavid77@gmail.com>
2023-10-10 18:48:09 +0100

    python async comprehension

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+measure runtime
+"""
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    start = time.perf_counter()
+    await asyncio.gather(async_comprehension())
+    finish = time.perf_counter() - start
+    return finish
