2e95f607eec6bde36f6dac86e9eb4a92c2690437
Nwosu David <nwosudavid77@gmail.com>
2025-05-14 23:46:59 +0100

    create table user_data

--- python-generators-0x00/seed.py
+++ python-generators-0x00/seed.py
@@ -14,6 +14,7 @@ def create_database(connection):
     "creates the database ALX_prodev if it does not exist"
     mycursor = connection.cursor()
     mycursor.execute("CREATE DATABASE ALX_prodev")
+
 def connect_to_prodev():
     "connects the the ALX_prodev database in MYSQL"
     return mysql.connector.connect(
@@ -22,7 +23,19 @@ def connect_to_prodev():
         password="password",
         database="ALX_prodev"
     )
+
 def create_table(connection):
     "creates a table user_data if it does not exists with the required fields"
+    cursor = connection.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS user_data (
+            id CHAR(36) PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            age INT NOT NULL,
+            email VARCHAR(255) NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+    """)
+
 def insert_data(connection, data):
     "inserts data in the database if it does not exist"
