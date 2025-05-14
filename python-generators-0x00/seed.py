b8fd0a76150a09622e0843d8810a0f9463ffc548
Nwosu David <nwosudavid77@gmail.com>
2025-05-14 23:28:46 +0100

    connection to database

---
+++ python-generators-0x00/seed.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import mysql.connector
+
+
+def connect_db():
+    "connects to the mysql database server"
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="password",
+    )
+
+def create_database(connection):
+    "creates the database ALX_prodev if it does not exist"
+    
+def connect_to_prodev():
+    "connects the the ALX_prodev database in MYSQL"
+def create_table(connection):
+    "creates a table user_data if it does not exists with the required fields"
+def insert_data(connection, data):
+    "inserts data in the database if it does not exist"
