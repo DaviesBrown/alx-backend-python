a568831ff7bd14d92769941d9eea1f264cc83269
Nwosu David <nwosudavid77@gmail.com>
2025-05-25 08:56:06 +0100

    fix db connection issues

--- python-decorators-0x01/1-with_db_connection.py
+++ python-decorators-0x01/1-with_db_connection.py
@@ -1,21 +1,22 @@
-import sqlite3 
+import sqlite3
 import functools
 
 def with_db_connection(func):
-    """handles opening and closing database connections"""
-    conn = sqlite3.connect('users.db')
-    def wrapper(user_id):
-        func(conn, user_id)
+    """Opens a new sqlite3 connection on each call, and closes it afterwards."""
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        conn = sqlite3.connect('users.db')
+        try:
+            return func(conn, *args, **kwargs)
+        finally:
+            conn.close()
     return wrapper
-    conn.close
 
-
-@with_db_connection 
-def get_user_by_id(conn, user_id): 
-    cursor = conn.cursor() 
-    cursor.execute("SELECT * FROM users WHERE id = ?", (user_id,)) 
-    return cursor.fetchone() 
-#### Fetch user by ID with automatic connection handling 
+@with_db_connection
+def get_user_by_id(conn, user_id):
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users WHERE id = ?", (user_id,))
+    return cursor.fetchone()
 
 user = get_user_by_id(user_id=1)
 print(user)
