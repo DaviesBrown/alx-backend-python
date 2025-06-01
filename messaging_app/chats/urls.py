fb6e4b10fe33fa8e93a28c8ce1a7c71ddba46650
Nwosu David <nwosudavid77@gmail.com>
2025-06-01 19:46:51 +0100

    Update url patterns to use router

--- messaging_app/chats/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets, routers
-from .views import ConversationViewSet, MessageViewSet
-router = routers.DefaultRouter()
-
-router.register(r'conversations', ConversationViewSet, basename='conversations')
-router.register(r'messages', MessageViewSet, basename='messages')
-
-urlpatterns = router.urls
